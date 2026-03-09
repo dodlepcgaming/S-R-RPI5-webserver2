60be236585a4c884e8acad189751087fce12cdeb
Mark <dodlepcgaming@gmail.com>
2026-03-09 13:11:00 -0400

    speamker

---
+++ MicINtoOUT.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+const Speaker = require('speaker');
+
+const wss = new WebSocket.Server({ port: 8080 });
+const speaker = new Speaker({
+  channels: 1,
+  bitDepth: 16,
+  sampleRate: 44100
+});
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', (data) => {
+    speaker.write(data);
+  });
+});
