2a021cd2587ae939d9bf1f30c0213c555ed23fff
Mark Drobyshev <dodlepcgaming@gmail.com>
2026-03-12 21:18:09 -0400

    speaker hopefully works

--- MicOut.js
+++ MicOut.js
@@ -1,17 +1,30 @@
 const WebSocket = require('ws');
-const Speaker = require('speaker');
+const { spawn } = require('child_process');
 
 const wss = new WebSocket.Server({ port: 8003 });
-const speaker = new Speaker({
-  channels: 1,
-  bitDepth: 16,
-  sampleRate: 44100
+
+const aplay = spawn('aplay', [
+  '-D', 'plughw:CARD=sndrpigooglevoi,DEV=0',
+  '-c', '1',
+  '-r', '44100',
+  '-f', 'S16_LE'
+]);
+
+aplay.stderr.on('data', (data) => {
+  console.error(`ALSA/aplay Status: ${data}`);
 });
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+  console.log('Client connected to Rover Speaker');
+  
   ws.on('message', (data) => {
-    speaker.write(data);
+    if (aplay.stdin.writable) {
+      aplay.stdin.write(data);
+    }
   });
+
+  ws.on('close', () => console.log('Client disconnected'));
 });
+
+console.log('Rover Audio Out listening on port 8003');
 
