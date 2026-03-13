2368ad6a17f33aa4f76186403b2dab57cd66917d
Mark Drobyshev <dodlepcgaming@gmail.com>
2026-03-12 23:07:30 -0400

    modified to make sure servos stop properly

--- ServoControl.py
+++ ServoControl.py
@@ -3,8 +3,8 @@ import websockets
 from gpiozero import Servo
 import json
 
-Y_servo = Servo(13, min_pulse_width=1/1000, max_pulse_width=2/1000)
-X_servo = Servo(12, min_pulse_width=1/1000, max_pulse_width=2/1000)
+Y_servo = Servo(13, initial_value=None,min_pulse_width=1/1000, max_pulse_width=2/1000)
+X_servo = Servo(12, initial_value=None, min_pulse_width=1/1000, max_pulse_width=2/1000)
 
 async def handle_websocket(websocket):
     print("Client connected to Pi 5 on port 8002")
@@ -25,6 +25,9 @@ async def handle_websocket(websocket):
             X_servo.value = -1.0
         else:              # STOP
             Y_servo.value, X_servo.value = 0.0, 0.0
+            await asyncio.sleep(0.1) 
+            Y_servo.value = None
+            X_servo.value = None
             
         print(f"Executing: {cmd}")
 
@@ -36,4 +39,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        Y_servo.value = X_servo.value = 0.0
+        Y_servo.value = X_servo.value = None
